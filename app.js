69ab9de98e74a0c71fc804bc188184d62349a041
Adam Preston <adam.m.preston@gmail.com>
2017-09-01 17:25:46 -0600

    put back some code

--- app.js
+++ app.js
@@ -108,8 +108,6 @@ app.post('/webhook', function (req, res) {
       var pageID = pageEntry.id;
       var timeOfEvent = pageEntry.time;
 
-      
-
       // Iterate over each messaging event
       pageEntry.messaging.forEach(function(messagingEvent) {
         if (messagingEvent.optin) {
